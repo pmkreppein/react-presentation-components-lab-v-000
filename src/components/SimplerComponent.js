360849ffc1b96e0f756e796f9fc7014103ec27ad
Peter Kreppein <pmkreppein@gmail.com>
2019-01-27 23:03:45 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const defaultLimit = 100
+
+export default function DumberComponent(props) {
+  return (
+    <div onClick={props.handleClick}>I am just happy.</div>
+  );
+}
