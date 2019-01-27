238a2b6015338905ef147b85519d1fb9a7d0aecc
Peter Kreppein <pmkreppein@gmail.com>
2019-01-27 23:03:34 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React from 'react'
+
+export default class DumbComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      mood: 'happy'
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    );
+  }
+}
